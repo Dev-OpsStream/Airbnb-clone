a30d50912a91da18887e03c760b619efd09457e0
Ramraj214 <ramrajnagapure54321@gmail.com>
2024-11-12 23:36:14 +0530

    helm updated

--- AirBnB/api/index.js
+++ AirBnB/api/index.js
@@ -26,7 +26,7 @@ if (!dbUrl || !secret) {
     process.exit(1);
 }
 
-main()
+mongoose.connect(process.env.MONGO_URI || "mongodb://mongo-service:27017/airbnb-db", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to DB");
     })
@@ -48,9 +48,10 @@ function getUserDataFromReq(req) {
 }
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
 }));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
